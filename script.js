40c560d1042db488fe7ae83e289fd5175671fe19
ayagabr21 <aya.gabr2000@gmail.com>
2021-10-22 18:04:14 +0200

    edit on accordion

--- script.js
+++ script.js
@@ -15,4 +15,23 @@ $(document).ready(function()
     })
 
 
+    // scroll top btn
+    // make the btn fade in when it reach 1000 and disappear before that
+    // consol.log(1000)
+    $(window).scroll(function(){
+        if($(this).scrollTop()>1000){
+            $(".top").fadeIn(150);
+            
+        }
+        else{
+            $(".top").fadeOut(150);
+        }
+    })
+    // make the scrolling up slower
+    // $(".top").click(function(){
+
+    //     $(".body").animate({scrollTop:0},150)
+        
+    // })
+
 })
